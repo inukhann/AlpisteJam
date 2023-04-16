bd1abfeceffb8fa8219bb7677fb932f1a5d37fab
InuInuREVOLUTION <109234729+InuInuREVOLUTION@users.noreply.github.com>
2023-04-16 14:51:29 -0300

    v0.2
    some movement added

--- LimbPunk/scripts/spark.gd
+++ LimbPunk/scripts/spark.gd
@@ -1,10 +1,17 @@
-extends Node
+extends CharacterBody2D
 
-# Called when the node enters the scene tree for the first time.
+var base_velocity = 10
+@onready var velocity_mod = 1
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
+func _physics_process(delta):
+	if Input.is_action_pressed("MOVE_RIGHT"):
+		set_velocity(Vector2.RIGHT * velocity_mod * base_velocity)
+	elif Input.is_action_pressed("MOVE_LEFT"):
+		set_velocity(Vector2.LEFT * velocity_mod * base_velocity)
+	else:
+		set_velocity(Vector2.ZERO)
+	move_and_slide()
+
