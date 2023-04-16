3e96756be2004c344160bae351ac8223573666c2
InuInuREVOLUTION <109234729+InuInuREVOLUTION@users.noreply.github.com>
2023-04-16 20:18:36 -0300

    v0.3
    more stuff added, no jump, test tile

--- LimbPunk/scripts/spark.gd
+++ LimbPunk/scripts/spark.gd
@@ -1,17 +1,25 @@
 extends CharacterBody2D
 
-var base_velocity = 10
+enum Status {
+	NORMAL, ARMLESS, LEGLESS, LIMBLESS, BODYLESS
+}
+
+var char_state
+var base_velocity = 100
 @onready var velocity_mod = 1
 
 func _ready():
-	pass
+	apply_central_force()
 
 func _physics_process(delta):
+	move_character()
+	move_and_slide()
+	
+
+func move_character():
 	if Input.is_action_pressed("MOVE_RIGHT"):
 		set_velocity(Vector2.RIGHT * velocity_mod * base_velocity)
 	elif Input.is_action_pressed("MOVE_LEFT"):
 		set_velocity(Vector2.LEFT * velocity_mod * base_velocity)
 	else:
 		set_velocity(Vector2.ZERO)
-	move_and_slide()
-
