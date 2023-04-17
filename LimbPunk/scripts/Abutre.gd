e293b36e1a44a8fbfab7b7a4f4d6f3b7246d7859
InuInuREVOLUTION <109234729+InuInuREVOLUTION@users.noreply.github.com>
2023-04-17 20:47:13 -0300

    v0.14.2
    more corrections to layers

--- LimbPunk/scripts/Abutre.gd
+++ LimbPunk/scripts/Abutre.gd
@@ -7,14 +7,16 @@ var speed = 150
 func _physics_process(delta):
 	if Global.player != null:
 		velocity = global_position.direction_to(Global.player.global_position)
-		
-		global_position += velocity * speed * delta
+		if can_attack:
+			global_position += velocity * speed * delta
 
 func _on_area_2d_body_entered(body):
-	if body.get_path() == "res://scenes/Spark.tscn" and can_attack:
+	if body.get_scene_file_path() == "res://scenes/Spark.tscn" and can_attack:
 		body.gameover()
-	elif body.get_path() == "res://scenes/Bullet.tscn":
+	elif body.get_scene_file_path() == "res://scenes/Bullet.tscn":
 		can_attack = false
+		$Stun_cooldown.start()
+
 
 
 func _on_stun_cooldown_timeout():
