4fe23c71bee3ea0d3acb11fe43f486e7aac0ea96
InuInuREVOLUTION <109234729+InuInuREVOLUTION@users.noreply.github.com>
2023-04-16 21:14:12 -0300

    v0.5
    basic movement and buran stuff

--- LimbPunk/scripts/spark.gd
+++ LimbPunk/scripts/spark.gd
@@ -4,27 +4,33 @@ enum Status {
 	NORMAL, ARMLESS, LEGLESS, LIMBLESS, BODYLESS
 }
 
-var char_state
-var base_velocity = 100
-@onready var velocity_mod = 1
+const base_velocity_x = 100
+const base_velocity_y = 100
+const GRAVITY = 10
+@onready var char_state = Status.NORMAL
+@onready var velocity_mod_x = 1
+@onready var velocity_mod_y = 1
 
-func _ready():
-	#apply_central_force()
-	pass
 
-func _physics_process(_delta):
-	move_character()
+func _physics_process(delta):
+	move_character_x()
+	move_character_y()
 	move_and_slide()
 	
 
-func move_character():
-	if Input.is_action_pressed("MOVE_RIGHT"):
-		set_velocity(Vector2.RIGHT * velocity_mod * base_velocity)
-	elif Input.is_action_pressed("MOVE_LEFT"):
-		set_velocity(Vector2.LEFT * velocity_mod * base_velocity)
-	else:
-		set_velocity(Vector2.ZERO)
-
-func gameover():
-	queue_free()
-	assert(get_tree().change_scene_to_file("res://scenes/gameover.tscn") == OK)
+func move_character_x():
+	if char_state != Status.LIMBLESS:
+		if Input.is_action_pressed("MOVE_RIGHT"):
+			velocity.x = (velocity_mod_x * base_velocity_x)
+		elif Input.is_action_pressed("MOVE_LEFT"):
+			velocity.x = (velocity_mod_x * -base_velocity_x)
+		else:
+			velocity.x = 0
+	
+func move_character_y():
+	if char_state == Status.NORMAL:
+		if Input.is_action_pressed("JUMP"):
+			velocity.y = (velocity_mod_y * -base_velocity_y)
+		else:
+			velocity.y += GRAVITY
+		
