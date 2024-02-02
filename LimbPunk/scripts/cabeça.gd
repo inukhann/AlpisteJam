2a430e1661ae08e3a70f647a869d7c68a7695275
Burandelei <92280548+dev-Burandelei@users.noreply.github.com>
2024-02-01 21:07:11 -0300

    Versão perdido+Bugfix campeonato

--- LimbPunk/scripts/cabeça.gd
+++ LimbPunk/scripts/cabeça.gd
@@ -24,6 +24,7 @@ func _on_area_2d_body_entered(_body):
 	
 	
 	
+	
 func shoot():
 	var projetil = ProjetilInstance.instantiate()
 	add_child(projetil)
@@ -43,3 +44,10 @@ func _on_timer_timeout():
 
 func _on_morre_desgraa_body_entered(_body):
 	queue_free()
+
+
+func _on_kill_player_body_entered(body):
+	if body.get_scene_file_path() == "res://scenes/Spark.tscn":
+		body.gameover()
+	elif body.get_scene_file_path() == "res://scenes/Bullet.tscn":
+		$Stun_cooldown.start()
--- LimbPunk/scripts/gameover.gd
+++ LimbPunk/scripts/gameover.gd
@@ -7,9 +7,20 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
 func _on_timer_timeout():
-	get_tree().change_scene_to_file("res://scenes/fase_2.tscn")
+	#get_tree().change_scene_to_file("res://scenes/fase_2.tscn")
+	pass
+
+func _on_resart_pressed():
+	if Global.fase == 1:
+		get_tree().change_scene_to_file("res://scenes/fase_1.tscn")
+	else: 
+		get_tree().change_scene_to_file("res://scenes/fase_2.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
